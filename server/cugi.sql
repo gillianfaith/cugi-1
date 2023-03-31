7cc96a16c259667363b5a783bcccd3ac15a8c14c
Mark Mosobo <34887895+markmosobo@users.noreply.github.com>
2023-03-31 04:49:43 +0300

    Updated cugi.sql

--- server/cugi.sql
+++ server/cugi.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 30, 2023 at 08:00 PM
--- Server version: 10.1.19-MariaDB
--- PHP Version: 5.5.38
+-- Generation Time: Mar 31, 2023 at 03:39 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.0.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -23,7 +24,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Table structure for table `pass_reset`
+--
+
+CREATE TABLE `pass_reset` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pass_reset`
+--
+
+INSERT INTO `pass_reset` (`id`, `email`, `token`) VALUES
+(14, 'mmosobo@gmail.com', 'ec25af6e42d356c455485cbc773f7f190f340a729ff8e27bc4e40647e66be9dbd1bb023c795b3f57fd16597d0334a186f888');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
@@ -31,30 +51,57 @@ CREATE TABLE `users` (
   `first_name` varchar(255) NOT NULL,
   `last_name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
+  `role_id` int(11) NOT NULL DEFAULT 1,
+  `status` int(11) NOT NULL DEFAULT 0,
+  `user_type` int(11) NOT NULL DEFAULT 1,
+  `organization_id` int(11) NOT NULL DEFAULT 1,
   `password` varchar(255) NOT NULL,
-  `user_type` int(200) NOT NULL,
-  `Log` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `modified_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `role_id`, `status`, `user_type`, `organization_id`, `password`, `created_at`, `modified_at`) VALUES
+(3, 'Charles', 'Nyakumbo', 'cnyakumbo@studentsforliberty.org', 1, 1, 1, 1, '81dc9bdb52d04dc20036dbd8313ed055', '2023-03-30 22:06:20', '2023-03-30 22:06:41'),
+(7, 'Mark', 'Mosobo', 'mmosobo@gmail.com', 1, 1, 1, 1, '49295e81560f25ecdb7db6d860a31f77', '2023-03-31 02:33:55', '2023-03-31 04:25:33');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `user`
+-- Indexes for table `pass_reset`
+--
+ALTER TABLE `pass_reset`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT for table `pass_reset`
+--
+ALTER TABLE `pass_reset`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
